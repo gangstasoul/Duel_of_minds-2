c60609c36b2da120f566b24723ccc43b4e847078
gangstasoul <30682890+gangstasoul@users.noreply.github.com>
2024-10-09 19:44:47 +0700

    Add files via upload

---
+++ files/library-player-2.js
@@ -0,0 +1,254 @@
+var word1 = document.createElement('img');word1.src = './Unit-4-media/1.png';word1.height = 150;
+var true1 = 'academy';
+var false1 = 'designer';
+var false1_2 = 'wire';
+var mean1; 
+var word2 = document.createElement('img');word2.src = './Unit-4-media/2.png';word2.height = 150;
+var true2 = 'technique';
+var false2 = 'power line';
+var false2_2 = 'underground';
+var mean2; 
+var word3 = document.createElement('img');word3.src = './Unit-4-media/3.png';word3.height = 150;
+var true3 = 'engaged';
+var false3 = 'wind turbine';
+var false3_2 = 'blade';
+var mean3; 
+var word4 = document.createElement('img');word4.src = './Unit-4-media/4.png';word4.height = 150;
+var true4 = 'motivated';
+var false4 = 'install';
+var false4_2 = 'base';
+var mean4; 
+var word5 = document.createElement('img');word5.src = './Unit-4-media/5.png';word5.height = 150;
+var true5 = 'program';
+var false5 = 'production';
+var false5_2 = 'facility';
+var mean5; 
+var word6 = document.createElement('img');word6.src = './Unit-4-media/6.png';word6.height = 150;
+var true6 = 'revolutionize';
+var false6 = 'wind turbine';
+var false6_2 = 'install';
+var mean6; 
+var word7 = document.createElement('img');word7.src = './Unit-4-media/7.png';word7.height = 150;
+var true7 = 'whole';
+var false7 = 'blade';
+var false7_2 = 'underground';
+var mean7; 
+var word8 = document.createElement('img');word8.src = './Unit-4-media/8.png';word8.height = 150;
+var true8 = 'sound';
+var false8 = 'wire';
+var false8_2 = 'wind farm';
+var mean8; 
+var word9 = document.createElement('img');word9.src = './Unit-4-media/9.png';word9.height = 150;
+var true9 = "sunset";
+var false9 = 'facility';
+var false9_2 = 'install';
+var mean9; 
+var word10 = document.createElement('img');word10.src = './Unit-4-media/10.png';word10.height = 150;
+var true10 = "context";
+var false10 = 'field trip';
+var false10_2 = 'production';
+var mean10;
+ 
+var word11 = document.createElement('img');word11.src = './Unit-4-media/11.png';word11.height = 150;
+var true11 = "water";
+var false11 = "blade";
+var false11_2 = 'wind turbine';
+var mean11; 
+var word12 = document.createElement('img');word12.src = './Unit-4-media/12.png';word12.height = 150;
+var true12 = "around";
+var false12 = "designer";
+var false12_2 = 'base';
+var mean12; 
+var word13 = document.createElement('img');word13.src = './Unit-4-media/13.png';word13.height = 150;
+var true13 = "changes";
+var false13 = "facility";
+var false13_2 = 'blade';
+var mean13; 
+var word14 = document.createElement('img');word14.src = './Unit-4-media/14.png';word14.height = 150;
+var true14 = "listen";
+var false14 = "wire";
+var false14_2 = 'underground';
+var mean14; 
+var word15 = document.createElement('img');word15.src = './Unit-4-media/1.png';word15.height = 150;
+var true15 = true1;
+var false15 = false1;
+var false15_2 = false1_2;
+var mean15;
+var word16 = document.createElement('img');word16.src = './Unit-4-media/2.png';word16.height = 150;
+var true16 = true2;
+var false16 = false2;
+var false16_2 = false2_2;
+var mean16;
+var word17 = document.createElement('img');word17.src = './Unit-4-media/3.png';word17.height = 150;
+var true17 = true3;
+var false17 = false3;
+var false17_2 = false3_2;
+var mean17;
+var word18 = document.createElement('img');word18.src = './Unit-4-media/4.png';word18.height = 150;
+var true18 = true4;
+var false18 = false4;
+var false18_2 = false4_2;
+var mean18; 
+var word19 = document.createElement('img');word19.src = './Unit-4-media/5.png';word19.height = 150;
+var true19 = true5;
+var false19 = false5;
+var false19_2 = false5_2;
+var mean19; 
+var word20 = document.createElement('img');word20.src = './Unit-4-media/6.png';word20.height = 150;
+var true20 = true6;
+var false20 = false6;
+var false20_2 = false6_2;
+var mean20; 
+var word21 = word1;
+var true21 = true1;
+var false21 = false1;
+var false21_2 = false1_2;
+var mean21; 
+var word22 = word2;
+var true22 = true2;
+var false22 = false2;
+var false22_2 = false2_2;
+var mean22; 
+var word23 = word3;
+var true23 = true3;
+var false23 = false3;
+var false23_2 = false3_2;
+var mean23; 
+var word24 = word4;
+var true24 = true4;
+var false24 = false4;
+var false24_2 = false4_2;
+var mean24; 
+var word25 = word5;
+var true25 = true5;
+var false25 = false5;
+var false25_2 = false5_2;
+var mean25; 
+var word26 = word6;
+var true26 = true6;
+var false26 = false6;
+var false26_2 = false6_2;
+var mean26; 
+var word27 = word7;
+var true27 = true7;
+var false27 = false7;
+var false27_2 = false7_2;
+var mean27; 
+var word28 = word8;
+var true28 = true8;
+var false28 = false8;
+var false28_2 = false8_2;
+var mean28; 
+var word29 = word9; 
+var true29 = true9;
+var false29 = false9;
+var false29_2 = false9_2;
+var mean29;
+var word30 = word10;
+var true30 = true10;
+var false30 = false10;
+var false30_2 = false10_2;
+var mean30; 
+var word31 = word11;
+var true31 = true11;
+var false31 = false11;
+var false31_2 = false11_2;
+var mean31; 
+var word32 = word12;
+var true32 = true12;
+var false32 = false12;
+var false32_2 = false12_2;
+var mean32; 
+var word33 = word13;
+var true33 = true13;
+var false33 = false13;
+var false33_2 = false13_2;
+var mean33; 
+var word34 = word14;
+var true34 = true14;
+var false34 = false14;
+var false34_2 = false14_2;
+var mean34; 
+var word35 = word15;
+var true35 = true15;
+var false35 = false15;
+var false35_2 = false15_2;
+var mean35; 
+var word36 = word16; 
+var true36 = true16;
+var false36 = false16;
+var false36_2 = false16_2;
+var mean36;
+var word37 = word17; 
+var true37 = true17;
+var false37 = false17;
+var false37_2 = false17_2;
+var mean37;
+var word38 = word18;
+var true38 = true18;
+var false38 = false18;
+var false38_2 = false18_2;
+var mean38; 
+var word39 = word19;
+var true39 = true19;
+var false39 = false19;
+var false39_2 = false19_2;
+var mean39; 
+var word40 = word20;
+var true40 = true20;
+var false40 = false20;
+var false40_2 = false20_2;
+var mean40;
+
+    var error = "error!!!";
+	var puzzle_sprint_words2 = 
+	{
+	"0":[
+	{"id":1,"diff":0,"word":word1,"visible_translation":error,"visible_translation2":error,"visible_translation3":error,"translation":true1,"fake_translation":false1,"fake_translation2":false1_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"adjective"},
+	{"id":2,"diff":0,"word":word2,"visible_translation":error,"visible_translation2":error,"visible_translation3":error,"translation":true2,"fake_translation":false2,"fake_translation2":false2_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":3,"diff":0,"word":word3,"visible_translation":error,"visible_translation2":error,"visible_translation3":error,"translation":true3,"fake_translation":false3,"fake_translation2":false3_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"adjective"},
+	{"id":4,"diff":0,"word":word4,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true4,"fake_translation":false4,"fake_translation2":false4_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"adjective"},
+	{"id":5,"diff":0,"word":word5,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true5,"fake_translation":false5,"fake_translation2":false5_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":6,"diff":0,"word":word6,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true6,"fake_translation":false6,"fake_translation2":false6_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":7,"diff":0,"word":word7,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true7,"fake_translation":false7,"fake_translation2":false7_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":8,"diff":0,"word":word8,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true8,"fake_translation":false8,"fake_translation2":false8_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":9,"diff":0,"word":word9,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true9,"fake_translation":false9,"fake_translation2":false9_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"pronoun"},
+	{"id":10,"diff":0,"word":word10,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true10,"fake_translation":false10,"fake_translation2":false10_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"adverb"},
+	{"id":11,"diff":0,"word":word11,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true11,"fake_translation": false11,"fake_translation2":false11_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"adjective"},
+	{"id":12,"diff":0,"word":word12,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true12,"fake_translation":false12,"fake_translation2":false12_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":13,"diff":0,"word":word13,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true13,"fake_translation":false13,"fake_translation2":false13_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"adjective"},
+	{"id":14,"diff":0,"word":word14,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true14,"fake_translation":false14,"fake_translation2":false14_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"adjective"},
+	{"id":15,"diff":0,"word":word15,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true15,"fake_translation":false15,"fake_translation2":false15_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":16,"diff":0,"word":word16,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true16,"fake_translation":false16,"fake_translation2":false16_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":17,"diff":0,"word":word17,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true17,"fake_translation":false17,"fake_translation2":false17_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":18,"diff":0,"word":word18,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true18,"fake_translation":false18,"fake_translation2":false18_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":19,"diff":0,"word":word19,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true19,"fake_translation":false19,"fake_translation2":false19_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"pronoun"},
+	{"id":20,"diff":0,"word":word20,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true20,"fake_translation":false20,"fake_translation2":false20_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"adverb"},
+	{"id":21,"diff":0,"word":word21,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true21,"fake_translation":false21,"fake_translation2":false21_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":22,"diff":0,"word":word22,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true22,"fake_translation":false22,"fake_translation2":false22_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":23,"diff":0,"word":word23,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true23,"fake_translation":false23,"fake_translation2":false23_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":24,"diff":0,"word":word24,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true24,"fake_translation":false24,"fake_translation2":false24_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":25,"diff":0,"word":word25,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true25,"fake_translation":false25,"fake_translation2":false25_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":26,"diff":0,"word":word26,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true26,"fake_translation":false26,"fake_translation2":false26_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":27,"diff":0,"word":word27,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true27,"fake_translation":false27,"fake_translation2":false27_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":28,"diff":0,"word":word28,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true28,"fake_translation":false28,"fake_translation2":false28_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":29,"diff":0,"word":word29,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true29,"fake_translation":false29,"fake_translation2":false29_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":30,"diff":0,"word":word30,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true30,"fake_translation":false30,"fake_translation2":false30_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"adjective"},
+	{"id":31,"diff":0,"word":word31,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true31,"fake_translation":false31,"fake_translation2":false31_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"adjective"},
+	{"id":32,"diff":0,"word":word32,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true32,"fake_translation":false32,"fake_translation2":false32_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"adjective"},
+	{"id":33,"diff":0,"word":word33,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true33,"fake_translation":false33,"fake_translation2":false33_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"adjective"},
+	{"id":34,"diff":0,"word":word34,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true34,"fake_translation":false34,"fake_translation2":false34_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":35,"diff":0,"word":word35,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true35,"fake_translation":false35,"fake_translation2":false35_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"adjective"},
+	{"id":36,"diff":0,"word":word36,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true36,"fake_translation":false36,"fake_translation2":false36_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"adjective"},
+	{"id":37,"diff":0,"word":word37,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true37,"fake_translation":false37,"fake_translation2":false37_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":38,"diff":0,"word":word38,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true38,"fake_translation":false38,"fake_translation2":false38_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":39,"diff":0,"word":word39,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true39,"fake_translation":false39,"fake_translation2":false39_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":40,"diff":0,"word":word40,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true40,"fake_translation":false40,"fake_translation2":false40_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	
+		
+	],	
+		};
+
+   var in_rating = '1';
+    var adjust_level = '0';
+	
